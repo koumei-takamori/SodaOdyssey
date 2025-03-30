6bd2107336ae3877c8dcdb5c8cdd5a5ac7c3c59e
koumei-takamori <koumeidesuyo1302@gmail.com>
2025-03-30 13:37:42 +0900

    Player追加　アセット追加

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,78 @@
+/**********************************************************
+ *
+ *  Player.cs
+ *  プレイヤー
+ *
+ *  制作者 : 髙森 煌明
+ *  制作日 : 2025/03/30
+ *
+ *********************************************************/
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerMove))]
+public class Player : MonoBehaviour
+{
+
+    // リジッドボディ
+    [SerializeField]
+    private Rigidbody2D m_rigidBody;
+
+    // プレイヤーデータ
+    [SerializeField]
+    private PlayerData m_playerData;
+
+    // 移動
+    private PlayerMove m_move;
+    // ステートマシン
+    private PlayerStateMachine m_playerStateMachine;
+    // プレイヤーステ―タス
+    private PlayerStatus m_playerStatus;
+
+    // プロパティ
+    public Rigidbody2D Rigidbody { get { return m_rigidBody; } }
+    public PlayerData PlayerData { get { return m_playerData; } }
+    public PlayerMove PlayerMove { get { return m_move; } }
+    public PlayerStateMachine StateMachine { get { return m_playerStateMachine; } }
+    public PlayerStatus PlayerStatus { get { return m_playerStatus; } }
+
+
+    // 実行前初期化処理
+    private void Awake()
+    {
+        //リジッドボディの取得
+        m_rigidBody = GetComponent<Rigidbody2D>();
+        //	移動の取得
+        m_move = GetComponent<PlayerMove>();
+
+        //	プレイヤーステータスの作成
+        m_playerStatus = new PlayerStatus();
+
+        //	ステートマシーンの作成
+        m_playerStateMachine = new PlayerStateMachine();
+
+        //	ステートマシーンの初期化	※初期状態は攻撃
+        m_playerStateMachine.Initialize(m_playerStateMachine.PlayerIdle);
+    }
+
+    // 初期化処理
+    private void Start()
+    {
+        
+    }
+
+    // 更新処理
+    private void Update()
+    {
+        //	ステートの更新
+        m_playerStateMachine.Update();
+
+        //	プレイヤーのステータスの更新
+        m_playerStatus.Update();
+
+
+        ////	死亡処理
+        //if (m_playerStatus.IsDie)
+        //    Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player/States/PlayerIdle.cs
@@ -0,0 +1,28 @@
+/**********************************************************
+ *
+ *  PlayerIdle.cs
+ *  プレイヤーの待機ステート
+ *
+ *  制作者 : 髙森 煌明
+ *  制作日 : 2025/03/30
+ *
+ *********************************************************/
+using UnityEngine;
+
+public class PlayerIdle : IPlayerState
+{
+
+    public void OnEnter()
+    {
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void OnExit()
+    {
+    }
+
+}
